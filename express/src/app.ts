908cb6ffe0131f72e4e5c446542601438c817172
Rodolfo <rodolfo.dartora@jeaholding.com.br>
2022-10-25 10:14:46 -0300

    first commit

--- express/src/app.ts
+++ express/src/app.ts
@@ -26,12 +26,12 @@ app.use('/users', usersRouter);
 app.use('/', rabbitmqTestRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -44,8 +44,10 @@ app.use(function(err, req, res, next) {
 const consumer = async () => {
   const server = new RabbitmqServer('amqp://admin:admin@rabbitmq:5672');
   await server.start();
-  await server.consume('express', (message) => console.log(message.content.toString()));
-}
+  await server.consume('express', (message) =>
+    console.log(message.content.toString())
+  );
+};
 
 consumer();
 
